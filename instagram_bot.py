bb85c62388c576a5c3def39045fad35c08202f19
mosajd <mosajd@gmail.com>
2020-11-18 21:24:30 +0330

    v.1.1

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,5 +1,6 @@
 # version 1.0
 # get followers from any user id
+# implementing pep8 for function names and function arguments blank spaces
 from selenium import webdriver
 import time
 from random import randint
@@ -10,15 +11,16 @@ from selenium.webdriver.common.action_chains import ActionChains
 # xpath link for followers popup
 followers_link = '//a[@class="-nal3 "]'
 followers = []
-class InstagramBot:
 
+
+class InstagramBot:
     def __init__(self, username, password, instagram_id):
         self.username = username
         self.password = password
         self.driver = webdriver.Chrome()
         self.instagram_id = instagram_id
 
-    def Login(self):
+    def login(self):
         self.driver.get('https://instagram.com')
         time.sleep(4)
         user_name = self.driver.find_element_by_xpath('//input[@name="username"]')
@@ -30,9 +32,9 @@ class InstagramBot:
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         time.sleep(5)
 
-    def DiscardPopup(self):
+    def discard_popup(self):
         # for get rid off from 'turn on notification' popup we can send to the following link
-        #alert = self.driver.switch_to.alert
+        # alert = self.driver.switch_to.alert
         try:
             self.driver.get(f'https://www.instagram.com/{self.instagram_id}/')
             time.sleep(5)
@@ -40,21 +42,21 @@ class InstagramBot:
             self.driver.find_element_by_xpath('//button[@class="aOOlW   HoLwm "]').click()
             time.sleep(3)
 
-        
-    def GetFollowersNumber(self):
-        followers_number = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span').get_attribute('title')
+    def get_followers_number(self):
+        followers_number = self.driver.find_element_by_xpath\
+            ('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span').get_attribute('title')
         time.sleep(3)
         print(followers_number)
-        #page_down_key_repetition = int(followers_number.replace(",", "")) // 12
-        #print(page_down_key_repetition)
+        # page_down_key_repetition = int(followers_number.replace(",", "")) // 12
+        # print(page_down_key_repetition)
 
-    def OpenLink(self,link):
+    def open_link(self,link):
         self.link = link
-        #to open the popup of followers
+        # to open the popup of followers
         self.driver.find_element_by_xpath(self.link).click()
         time.sleep(5)
 
-    def PressTabKey(self,repetition,time_sleep):
+    def press_tab_key(self, repetition, time_sleep):
         self.repetition = repetition
         self.time_sleep = time_sleep
         # number of times you want to press the key
@@ -64,7 +66,7 @@ class InstagramBot:
             print(_)
             time.sleep(self.time_sleep)
 
-    def PressPageDownKey(self,repetition,time_sleep):
+    def press_page_down_key(self, repetition, time_sleep):
         self.repetition = repetition
         self.time_sleep = time_sleep
         # number of times you want to press the key
@@ -74,55 +76,53 @@ class InstagramBot:
             print(_)
             time.sleep(self.time_sleep)
         
-        #down_action.send_keys(Keys.ARROW_DOWN).perform()
-         #time.sleep(2)
-        #down_action = ActionChains(self.driver) 
-        #for y in range(1):
+        # down_action.send_keys(Keys.ARROW_DOWN).perform()
+         # time.sleep(2)
+        # down_action = ActionChains(self.driver)
+        # for y in range(1):
            # down_action.send_keys(Keys.PAGE_DOWN).perform()
-          #  time.sleep(6)
-        #actions.send_keys(Keys.TAB).perform()
-        #time.sleep(5)  
+          # time.sleep(6)
+        # actions.send_keys(Keys.TAB).perform()
+        # time.sleep(5)
 
-    def CollectFollowers(self):
+    def collect_followers(self):
         
-        for i in range(1,30):                                                                 
-            temp = self.driver.find_element_by_xpath(f'/html/body/div[4]/div/div/div[2]/ul/div/li[{i}]/div/div[1]/div[2]/div[1]/span/a').get_attribute('title')
+        for i in range(1, 30):
+            temp = self.driver.find_element_by_xpath(f'/html/body/div[4]/div/div/div[2]/ul/div/li[{i}]'
+                                                     f'/div/div[1]/div[2]/div[1]/span/a').get_attribute('title')
             if temp not in followers:                 
                 followers.append(temp)                 
-            print('%s: %s' % (i,temp))                
+            print('%s: %s' % (i, temp))
             time.sleep(0.01)    
 
-    def PrintResults(self):                        
-        for _ in range(0,len(followers)):
-            print('%s: %s' % (_+1,followers[_]))
+    def print_results(self):
+        for _ in range(0, len(followers)):
+            print('%s: %s' % (_+1, followers[_]))
         print("The length of followers are: ")
         print(len(followers))
         print(followers)
 
-    def Quit(self):
+    def quit(self):
         self.driver.quit()
+            # self.driver.execute_script('scroll(0,400)')
+            # time.sleep(3)
+        # followers_id = []
+        # alert = self.driver.switch_to.alert
+        # followers = alert.find_elements_by_xpath('//a[@class="FPmhX notranslate  _0imsa "]')
+        # for i in range(len(followers)):
+            # followers_id = followers[i].get_attribute('href')
+        # print(len(followers_id))
 
-            #self.driver.execute_script('scroll(0,400)')   
-            #time.sleep(3)                               
-        #followers_id = []                             
-        #alert = self.driver.switch_to.alert
-        #followers = alert.find_elements_by_xpath('//a[@class="FPmhX notranslate  _0imsa "]')
-        #for i in range(len(followers)):
-            #followers_id = followers[i].get_attribute('href')
 
-
-            
-        #print(len(followers_id))
-
-test = InstagramBot(username, password, instagram_id)
-test.Login()
-test.DiscardPopup()
-test.GetFollowersNumber()
-test.OpenLink(followers_link)
-test.PressTabKey(2, 3)
-test.PressPageDownKey(4, 7)
-test.PressTabKey(1, 6)
-test.CollectFollowers()
-test.PrintResults()
-test.Quit
+test = InstagramBot("username", "password", "instagram_id")
+test.login()
+test.discard_popup()
+test.get_followers_number()
+test.open_link(followers_link)
+test.press_tab_key(2, 3)
+test.press_page_down_key(4, 7)
+test.press_tab_key(1, 6)
+test.collect_followers()
+test.print_results()
+test.quit()
     
